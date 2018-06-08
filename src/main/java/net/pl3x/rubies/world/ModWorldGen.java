069467035c8b445659da4ed6dcf1ef1d16609253
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-06-08 09:57:28 -0500

    Make ruby ore generation configurable

--- src/main/java/net/pl3x/rubies/world/ModWorldGen.java
+++ src/main/java/net/pl3x/rubies/world/ModWorldGen.java
@@ -1,22 +1,28 @@
 package net.pl3x.rubies.world;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
 import net.pl3x.rubies.block.ModBlocks;
+import net.pl3x.rubies.configuration.RubyConfig;
 
 import java.util.Random;
 
 public class ModWorldGen implements IWorldGenerator {
     @Override
     public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-        if (world.provider.getDimension() == 0) {
-            new WorldGenMinable(ModBlocks.RUBY_ORE.getDefaultState(), rand.nextInt(6))
-                    .generate(world, rand, new BlockPos((chunkX << 4) + rand.nextInt(11),
-                            rand.nextInt(16), (chunkZ << 4) + rand.nextInt(16)));
+        if (RubyConfig.oreChance > 0 && RubyConfig.oreVeinSize > 0 && world.provider.getDimensionType() == DimensionType.OVERWORLD) {
+            System.out.println("ding! " + System.currentTimeMillis());
+            WorldGenMinable generator = new WorldGenMinable(ModBlocks.RUBY_ORE.getDefaultState(), rand.nextInt(RubyConfig.oreVeinSize));
+            int yDiff = RubyConfig.oreMaxY - RubyConfig.oreMinY + 1;
+            for (int i = 0; i < RubyConfig.oreChance; i++) {
+                generator.generate(world, rand, new BlockPos((chunkX << 4) + rand.nextInt(16),
+                        RubyConfig.oreMinY + rand.nextInt(yDiff), (chunkZ << 4) + rand.nextInt(16)));
+            }
         }
     }
 }
